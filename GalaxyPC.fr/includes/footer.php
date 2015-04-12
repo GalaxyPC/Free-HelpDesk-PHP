463976ec0467085f5b985fe95d924b91d98e582b
Benjamin <benjic78@hotmail.fr>
2015-04-12 13:47:58 +0200

    Version modifier

---
+++ GalaxyPC.fr/includes/footer.php
@@ -0,0 +1,7 @@
+<p><a href="http://galaxypc.fr/" target="_blank" title="Site internet GalaxyPC"><img src="img/fhd-logo.png" alt="galaxypc.fr " width="60" height="59" border="0" style="float: right;"></a></p>
+</div>
+<script src="js/parsley.min.js"></script>
+<script src="js/bootstrap-alert.js"></script> 
+
+</body>
+</html>
